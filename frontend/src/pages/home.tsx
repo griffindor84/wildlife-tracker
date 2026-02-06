cea2a6b21b8c9877da8f7e4b070df889d599f1fb
ian <mathaiian9@gmail.com>
2026-02-06 17:40:43 +0300

    Initial commit: add natgeo project

---
+++ frontend/src/pages/home.tsx
@@ -0,0 +1,9 @@
+function HomePage() {
+    return (
+        <div>
+            <h1>Welcome to the Wildlife Tracker App</h1>
+            <p>Track and monitor wildlife sightings in your area.</p>
+        </div>
+    );
+}
+export default HomePage;
