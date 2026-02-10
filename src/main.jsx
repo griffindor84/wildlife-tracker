bb159e7799c9b02ec3245a0b4a7d09b38287a3cb
ian <mathaiian9@gmail.com>
2026-02-10 22:01:06 +0300

    Update gitignore and main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+
+import App from './App.js'
 import { BrowserRouter } from 'react-router-dom';
 import 'leaflet/dist/leaflet.css';
 
 
-createRoot(document.getElementById('root')!).render(
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Root element not found');
+
+createRoot(rootElement).render(
   <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
