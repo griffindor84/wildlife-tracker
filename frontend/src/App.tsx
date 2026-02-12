ec749d14374a9720290ea6f068c8a6f251a78db6
Enock Tai <enocktai241@gmail.com>
2026-02-12 09:58:49 +0300

    fixed admin panel

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,7 +10,12 @@ import Register from './pages/Register';
 import UserProfile from './pages/UserProfile';
 import type { User } from './types';
 import Home from './pages/home';
-import AdminLayout from './admin/AdminLayout'; 
+import AdminLayout from './admin/AdminLayout';
+import Dashboard from './admin/Dashboard';
+import Settings from './admin/Settings';
+import Wildlife from './admin/Wildlife';
+import Users from './admin/Users';
+import AdminReports from './admin/AdminReports';
 
 function AppRoutes() {
   const [user, setUser] = useState<User | null>(null);
@@ -40,7 +45,15 @@ function AppRoutes() {
         <Route path="/reports" element={user ? <Reports /> : <Navigate to="/login" />} />
         <Route path="/species" element={user ? <Species /> : <Navigate to="/login" />} />
         <Route path="/observations" element={user ? <Observations /> : <Navigate to="/login" />} />
-        <Route path="/admin/*" element={user ? <AdminLayout /> : <Navigate to="/login" />} />
+        <Route path="/admin" element={user ? <AdminLayout /> : <Navigate to="/login" />}>
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="users" element={<Users />} />
+          <Route path="wildlife" element={<Wildlife />} />
+          <Route path="reports" element={<AdminReports />} />
+          <Route path="settings" element={<Settings />} />
+        </Route>
+
+
         <Route path="/profile" element={user ? <UserProfile user={user} onLogout={handleLogout} /> : <Navigate to="/login" />} />
         <Route path="/login" element={<Login onLogin={handleLogin} />} />
         <Route path="/register" element={<Register onRegister={handleRegister} />} />
